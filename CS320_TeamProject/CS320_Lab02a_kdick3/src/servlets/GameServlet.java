c84af97183cbb18d07c14c4af9f120814b54f24c
Korbin-Dick <kdick3@ycp.edu>
2024-03-13 18:06:08 -0400

    I got night ass but this is where im at so far, havennt implmented my html scrolling box yet
    i set some packages up in the lab02a java project to try and i guess view servlets in there and did a game jsp and web.xml but yet to have it wokring

---
+++ CS320_TeamProject/CS320_Lab02a_kdick3/src/servlets/GameServlet.java
@@ -0,0 +1,24 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		System.out.println("Game Servlet: doGet");	
+		
+		// call JSP to generate empty form
+		req.getRequestDispatcher("/_view/game.jsp").forward(req, resp);
+	}
+}
+	
+	
