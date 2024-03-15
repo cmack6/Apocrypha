52041346003ceac21d8a40b28ec5dd93376f68bf
cmack6 <cmack6@ycp.edu>
2024-03-14 22:58:37 -0400

    login page working
    everything without a create account page is there

---
+++ CS320_TeamProject/CS320_Lab02a_kdick3/src/servlets/LoginServlet.java
@@ -0,0 +1,62 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controllers.GameEngineController;
+import models.GameModel;
+import models.LoginModel;
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Login Servlet: doGet");
+		
+		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Login Servlet: doPost");
+		
+		
+		LoginModel model = new LoginModel();
+		model.setUsername(req.getParameter("username"));
+		model.setPassword(req.getParameter("password"));
+		String testUsername = "1";
+		
+		String testPassword = "2";
+		
+		String errorMessage = null;
+	
+		if(model.getUsername().equals(testUsername)&&model.getPassword().equals(testPassword)) {
+			resp.sendRedirect("http://localhost:8081/lab02/account");
+		}
+		else {
+			req.setAttribute("username",model.getUsername());
+			errorMessage = "Invalid credentials.";
+		}
+		
+		
+		
+		req.setAttribute("login",model);
+		req.setAttribute("errorMessage", errorMessage);
+		
+		
+		
+		
+		
+		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+		
+	}
+}
