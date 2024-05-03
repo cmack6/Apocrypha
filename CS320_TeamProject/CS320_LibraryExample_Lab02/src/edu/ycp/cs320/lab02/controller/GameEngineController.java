844d2443feada406d5c341b79d3fb22179c8921f
spencer-goated <160776722+spencer-goated@users.noreply.github.com>
2024-05-03 13:24:05 -0400

    you spelled poison wrong
    should fix it

--- CS320_TeamProject/CS320_LibraryExample_Lab02/src/edu/ycp/cs320/lab02/controller/GameEngineController.java
+++ CS320_TeamProject/CS320_LibraryExample_Lab02/src/edu/ycp/cs320/lab02/controller/GameEngineController.java
@@ -913,7 +913,7 @@ import edu.ycp.cs320.booksdb.persist.IDatabase;
 			int physicalResistance = 0;
 			int forceResistance = 0;
 			int necroticResistance = 0;
-			int poisionResistance = 0;
+			int poisonResistance = 0;
 			int thunderResistance = 0;
 			int fireResistance = 0;
 			
@@ -961,8 +961,8 @@ import edu.ycp.cs320.booksdb.persist.IDatabase;
 			else if(equippedEquipment.get(i).getArmorType().equals("Necrotic")) {
 				necroticResistance  = necroticResistance + equippedEquipment.get(i).getDefenseNumber();
 			}
-			else if(equippedEquipment.get(i).getArmorType().equals("Poision")) {
-				poisionResistance  = poisionResistance + equippedEquipment.get(i).getDefenseNumber();
+			else if(equippedEquipment.get(i).getArmorType().equals("")) {
+				poisonResistance  = poisonResistance + equippedEquipment.get(i).getDefenseNumber();
 			}
 			else if(equippedEquipment.get(i).getArmorType().equals("Thunder")) {
 				thunderResistance  = thunderResistance + equippedEquipment.get(i).getDefenseNumber();
@@ -977,7 +977,7 @@ import edu.ycp.cs320.booksdb.persist.IDatabase;
 					+	"<p>" + "Radiant Resistance: " + radiantResistance + "</p>"
 					+	"<p>" + "Force Resistance: " + forceResistance + "</p>"
 					+	"<p>" + "Necrotic Resistance: " + necroticResistance + "</p>"
-					+	"<p>" + "Poision Resistance: " + poisionResistance + "</p>"
+					+	"<p>" + "Poison Resistance: " + poisonResistance + "</p>"
 					+	"<p>" + "Thunder Resistance: " + thunderResistance + "</p>"
 					+	"<p>" + "Fire Resistance: " + fireResistance + "</p>"
 					+	"<p>" + "Equipped Mainhand Damage Type: " + currentDamageTypeMainHand + "</p>"
